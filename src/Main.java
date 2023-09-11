a95e9dd0deaae9767ca4d112c048a74d00be2485
cassie41589 <103098387+cassie41589@users.noreply.github.com>
2023-09-11 10:31:12 -0400

    Main changes

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        FizzBuzz.doFizzBuzz(5);
+        // do this to call the main of another class
+        FizzBuzz.main(null);
+    }
+
+}
