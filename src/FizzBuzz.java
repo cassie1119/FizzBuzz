f59d69a782f9977d095bf9c3a42458ea8c055a66
cassie41589 <103098387+cassie41589@users.noreply.github.com>
2023-09-11 09:53:49 -0400

    refactored to extract helper method + added why

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,34 +1,63 @@
 /**
  * Solve the FizzBuzz challenge.
  */
+// can add public
+// and in java = &&
 class FizzBuzz {
 
     public static void main(String[] args) {
+        // for (start; end; condition of increment)
+//        for (int i = 1; i < 100; i++) {
+//
+//            doFizzBuzz(i);
+//        }
 
-        for (int i = 1; i < 100; i++) {
+        int j = 1;
+        while (j < 100) {
+            whydoFizzBuzz(j);
+            j++;
+        }
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+    }
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+    private static void doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
 
-                System.out.println("Fizz Buzz");
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
 
-            } else if (divisibleBy3) {
+            System.out.println("Fizz Buzz");
 
-                System.out.println("Fizz");
+        } else if (divisibleBy3) {
 
-            } else if (divisibleBy5) {
+            System.out.println("Fizz");
 
-                System.out.println("Buzz");
+        } else if (divisibleBy5) {
 
-            } else {
+            System.out.println("Buzz");
 
-                System.out.println(i);
+        } else {
 
-            }
+            System.out.println(i);
+
         }
     }
+
+    private static void whydoFizzBuzz(int j) {
+        boolean divBy3 = j % 3 == 0;
+        boolean divBy5 = j % 5 == 0;
+        if (divBy5 && divBy3) {
+            System.out.println("Fizz Buzz");
+        } else if (divBy3) {
+            System.out.println("Fizz");
+        } else if (divBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(j);
+        }
+    }
+
+
 }
